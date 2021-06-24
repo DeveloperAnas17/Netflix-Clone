44e0a159d2ab851ad6f5676e0eb657daffaf9089
Anas <amanabbasi3660@gmail.com>
2021-06-24 16:10:15 +0530

    add banner

--- src/components/Originals.js
+++ src/components/Originals.js
@@ -1,39 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import styled from "styled-components";
 
-function Originals() {
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Originals({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <Container>
-      <h4>New To Disney</h4>
+      <h2>{title}</h2>
       <Content>
-        <Wrap>
-          <img
-            src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
-            alt=""
-          />
-        </Wrap>
-
-        <Wrap>
-          <img
-            src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
-            alt=""
-          />
-        </Wrap>
-
-        <Wrap>
-          <img
-            src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
-            alt=""
-          />
-        </Wrap>
-
-        <Wrap>
-          <img
-            src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
-            alt=""
-          />
-        </Wrap>
+        {movies.map((movie) => (
+          <Wrap>
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          </Wrap>
+        ))}
       </Content>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
     </Container>
   );
 }
@@ -41,11 +69,12 @@ function Originals() {
 const Container = styled.div`
   padding-top: 15px;
 
-  h4 {
+  h2 {
     color: #f1f4ff;
     margin-left: 22px;
   }
 `;
+
 const Content = styled.div`
   margin: 20px 0;
   padding: 10px 20px;
@@ -60,7 +89,7 @@ const Content = styled.div`
 `;
 
 const Wrap = styled.div`
-  padding-top: 56.25%;
+  padding-top: 60%;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
@@ -80,6 +109,16 @@ const Wrap = styled.div`
     width: 100%;
     z-index: 1;
     top: 0;
+
+    .row-poster {
+      object-fit: contain;
+      width: 100%;
+      max-height: 100px;
+      margin-right: 10px;
+      transition: transform 450ms;
+      min-height: 250px;
+      transition: transform 450ms;
+    }
   }
 
   &:hover {
@@ -89,5 +128,4 @@ const Wrap = styled.div`
     border-color: rgba(249, 249, 249, 0.8);
   }
 `;
-
 export default Originals;
